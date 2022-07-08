8708d9fcc581dfc456d08f3cec0f339011d8c7d6
Kevin Byron <kwbyron@gmail.com>
2022-07-08 23:51:52 +0900

    Readability and packaging changes
    * autoformated code
    * added package namespacing to replace require()
    * replaced options(warn=-1)/warn=0 with suppressWarnings()
    * User supplies formated dataframe instead of file path
    * EG_DF default is now a package data object

--- R/EQR.R
+++ R/EQR.R
@@ -1,8 +1,18 @@
+#' EQR
+#'
+#' @param data dataframe
+#'
+#' @return
+#'
+#' @examples
+
 EQR <- function(data) {
-  segm <- data[nrow(data),] - data[(nrow(data)-1),]
+  segm <- data[nrow(data), ] - data[(nrow(data) - 1), ]
   vett <- matrix(NA, nrow = nrow(data), ncol = ncol(data))
-  for (k in 1: ncol(data)) {vett[, k] <- data[(nrow(data)-1), k]}
+  for (k in 1:ncol(data)) {
+    vett[, k] <- data[(nrow(data) - 1), k]
+  }
   vett <- data - vett
-  ris <- round((vett %*% segm / sqrt(sum(segm*segm))) / sqrt(sum(segm*segm)), 3)
+  ris <- round((vett %*% segm / sqrt(sum(segm * segm))) / sqrt(sum(segm * segm)), 3)
   return(ris)
 }
